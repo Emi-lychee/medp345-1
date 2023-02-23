bce157bedbe8edc80f84344db382de127a993a74
Emily Wu <99505073+Emi-lychee@users.noreply.github.com>
2023-02-22 23:32:37 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,265 @@
+// Vivi Bubble Tea Menu
+
+var modal1 = document.getElementById("myModal1");
+
+var img1 = document.getElementById("myImg1");
+var modalImg1 = document.getElementById("img01");
+var captionText1 = document.getElementById("caption1");
+img1.onclick = function(){
+	modal1.style.display = "block";
+	modalImg1.src = this.src;
+	captionText1.innerHTML = this.alt;
+}
+
+var span1 = document.getElementsByClassName("close1")[0];
+
+span1.onclick = function() {
+	modal1.style.display = "none";
+}
+
+// Gong Cha Regular Tapioca Topping
+
+var modal2 = document.getElementById("myModal2");
+
+var img2 = document.getElementById("myImg2");
+var modalImg2 = document.getElementById("img02");
+var captionText2 = document.getElementById("caption2");
+img2.onclick = function(){
+	modal2.style.display = "block";
+	modalImg2.src = this.src;
+	captionText2.innerHTML = this.alt;
+}
+
+var span2 = document.getElementsByClassName("close2")[0];
+
+span2.onclick = function() {
+	modal2.style.display = "none";
+}
+
+// Gong Cha Coconut Jelly Topping
+
+var modal3 = document.getElementById("myModal3");
+
+var img3 = document.getElementById("myImg3");
+var modalImg3 = document.getElementById("img03");
+var captionText3 = document.getElementById("caption3");
+img3.onclick = function(){
+	modal3.style.display = "block";
+	modalImg3.src = this.src;
+	captionText3.innerHTML = this.alt;
+}
+
+var span3 = document.getElementsByClassName("close3")[0];
+
+span3.onclick = function() {
+	modal3.style.display = "none";
+}
+
+// Gong Cha Herbal Jelly Topping
+
+var modal4 = document.getElementById("myModal4");
+
+var img4 = document.getElementById("myImg4");
+var modalImg4 = document.getElementById("img04");
+var captionText4 = document.getElementById("caption4");
+img4.onclick = function(){
+	modal4.style.display = "block";
+	modalImg4.src = this.src;
+	captionText4.innerHTML = this.alt;
+}
+
+var span4 = document.getElementsByClassName("close4")[0];
+
+span4.onclick = function() {
+	modal4.style.display = "none";
+}
+
+// Gong Cha Aiyu Jelly Topping
+
+var modal5 = document.getElementById("myModal5");
+
+var img5 = document.getElementById("myImg5");
+var modalImg5 = document.getElementById("img05");
+var captionText5 = document.getElementById("caption5");
+img5.onclick = function(){
+	modal5.style.display = "block";
+	modalImg5.src = this.src;
+	captionText5.innerHTML = this.alt;
+}
+
+var span5 = document.getElementsByClassName("close5")[0];
+
+span5.onclick = function() {
+	modal5.style.display = "none";
+}
+
+// Gong Cha Star Jelly Topping
+
+var modal6 = document.getElementById("myModal6");
+
+var img6 = document.getElementById("myImg6");
+var modalImg6 = document.getElementById("img06");
+var captionText6 = document.getElementById("caption6");
+img6.onclick = function(){
+	modal6.style.display = "block";
+	modalImg6.src = this.src;
+	captionText6.innerHTML = this.alt;
+}
+
+var span6 = document.getElementsByClassName("close6")[0];
+
+span6.onclick = function() {
+	modal6.style.display = "none";
+}
+
+// Gong Cha Mango Popping Topping
+
+var modal7 = document.getElementById("myModal7");
+
+var img7 = document.getElementById("myImg7");
+var modalImg7 = document.getElementById("img07");
+var captionText7 = document.getElementById("caption7");
+img7.onclick = function(){
+	modal7.style.display = "block";
+	modalImg7.src = this.src;
+	captionText7.innerHTML = this.alt;
+}
+
+var span7 = document.getElementsByClassName("close7")[0];
+
+span7.onclick = function() {
+	modal7.style.display = "none";
+}
+
+// Gong Cha White Pearl Topping
+
+var modal8 = document.getElementById("myModal8");
+
+var img8 = document.getElementById("myImg8");
+var modalImg3 = document.getElementById("img08");
+var captionText8 = document.getElementById("caption8");
+img8.onclick = function(){
+	modal8.style.display = "block";
+	modalImg8.src = this.src;
+	captionText8.innerHTML = this.alt;
+}
+
+var span8 = document.getElementsByClassName("close8")[0];
+
+span8.onclick = function() {
+	modal8.style.display = "none";
+}
+
+// Gong Cha Strawberry Pearl Topping
+
+var modal9 = document.getElementById("myModal9");
+
+var img9 = document.getElementById("myImg9");
+var modalImg9 = document.getElementById("img09");
+var captionText9 = document.getElementById("caption9");
+img9.onclick = function(){
+	modal9.style.display = "block";
+	modalImg9.src = this.src;
+	captionText9.innerHTML = this.alt;
+}
+
+var span9 = document.getElementsByClassName("close9")[0];
+
+span9.onclick = function() {
+	modal9.style.display = "none";
+}
+
+// Gong Cha Pudding Topping
+
+var modal10 = document.getElementById("myModal10");
+
+var img10 = document.getElementById("myImg10");
+var modalImg10 = document.getElementById("img10");
+var captionText10 = document.getElementById("caption10");
+img10.onclick = function(){
+	modal10.style.display = "block";
+	modalImg10.src = this.src;
+	captionText10.innerHTML = this.alt;
+}
+
+var span10 = document.getElementsByClassName("close10")[0];
+
+span10.onclick = function() {
+	modal10.style.display = "none";
+}
+
+// Gong Cha Lemon Syrup Topping
+
+var modal11 = document.getElementById("myModal11");
+
+var img11 = document.getElementById("myImg11");
+var modalImg11 = document.getElementById("img11");
+var captionText11 = document.getElementById("caption11");
+img11.onclick = function(){
+	modal11.style.display = "block";
+	modalImg11.src = this.src;
+	captionText11.innerHTML = this.alt;
+}
+
+var span11 = document.getElementsByClassName("close11")[0];
+
+span11.onclick = function() {
+	modal11.style.display = "none";
+}
+
+// Gong Cha Pineapple Syrup Topping
+
+var modal12 = document.getElementById("myModal12");
+
+var img12 = document.getElementById("myImg12");
+var modalImg12 = document.getElementById("img12");
+var captionText12 = document.getElementById("caption12");
+img12.onclick = function(){
+	modal12.style.display = "block";
+	modalImg12.src = this.src;
+	captionText12.innerHTML = this.alt;
+}
+
+var span12 = document.getElementsByClassName("close12")[0];
+
+span12.onclick = function() {
+	modal12.style.display = "none";
+}
+
+// Gong Cha Basil Seeds Topping
+
+var modal13 = document.getElementById("myModal13");
+
+var img13 = document.getElementById("myImg13");
+var modalImg13 = document.getElementById("img13");
+var captionText13 = document.getElementById("caption13");
+img13.onclick = function(){
+	modal13.style.display = "block";
+	modalImg13.src = this.src;
+	captionText13.innerHTML = this.alt;
+}
+
+var span13 = document.getElementsByClassName("close13")[0];
+
+span13.onclick = function() {
+	modal13.style.display = "none";
+}
+
+// Gong Cha Ice & Sugar Levels
+
+var modal14 = document.getElementById("myModal14");
+
+var img14 = document.getElementById("myImg14");
+var modalImg14 = document.getElementById("img14");
+var captionText14 = document.getElementById("caption14");
+img14.onclick = function(){
+	modal14.style.display = "block";
+	modalImg14.src = this.src;
+	captionText14.innerHTML = this.alt;
+}
+
+var span14 = document.getElementsByClassName("close14")[0];
+
+span14.onclick = function() {
+	modal14.style.display = "none";
+}
